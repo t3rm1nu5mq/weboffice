455efd25ad7851053072791c57d1883d0b3604ca
Szabó András Ferenc <terminusmq@outlook.com>
2019-09-02 01:52:45 +0200

    Localization
    - hu & en strings added
    - default language: hu

---
+++ resources/lang/en/strings.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Basic language lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the application.
+    |
+    */
+
+    'login' => 'Login',
+    'register' => 'Register'
+
+];
