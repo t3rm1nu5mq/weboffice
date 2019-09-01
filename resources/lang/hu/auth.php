efee1f04a52497432c0f1f20ca4deddc7ad5dc55
Szabó András Ferenc <terminusmq@outlook.com>
2019-09-01 02:15:36 +0200

    Hungarian language added

---
+++ resources/lang/hu/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'A megadott e-mail-cím/jelszó hibás.',
+    'throttle' => 'Túl sok bejelentkezési kísérlet. Kérjük, próbálja meg újra :seconds másodperc múlva.',
+
+];
