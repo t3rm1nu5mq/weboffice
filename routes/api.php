1d8195f342ccd57cf29498dc35452f63c33f5a08
Szabó András Ferenc <terminusmq@outlook.com>
2019-11-29 00:44:47 +0100

    Partners wireframe

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('payments', 'API\PaymentController');
+
+Route::resource('partners', 'API\PartnerController');
