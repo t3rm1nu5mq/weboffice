12d95a81119dab2e6482d1354b39b579db8aa862
Szabó András Ferenc <terminusmq@outlook.com>
2019-12-25 01:40:36 +0100

    Dashboard edited
    - UI mods
      * v-card cursor
      * v-card title center aligned
      * v-card data font-size
    - partners count dinamically get

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -15,7 +15,7 @@
                             shaped
                         >
                             <v-card-title class="headline">Projektek</v-card-title>
-                            <v-card-text class="text-center font-weight-bold">1</v-card-text>
+                            <v-card-text class="text-center font-weight-bold card-data">1</v-card-text>
                         </v-card>
                     </v-col>
                     <v-col sm="2">
@@ -25,7 +25,7 @@
                             shaped
                         >
                             <v-card-title class="headline">Partnerek</v-card-title>
-                            <v-card-text class="text-center font-weight-bold">1</v-card-text>
+                            <v-card-text class="text-center font-weight-bold card-data">{{ partners }}</v-card-text>
                         </v-card>
                     </v-col>
                 </v-row>
@@ -37,11 +37,40 @@
 <script>
     import layout from './Layout';
 
+
     export default {
         name: 'Dashboard',
 
         components: {
             layout
+        },
+
+        created() {
+            const that = this;
+            axios.get('/api/partners_count').then(function(response) {
+                that.partners = response.data.data;
+            }).catch(function(error) {
+                console.log(error);
+            });
+        },
+
+        data() {
+            return {
+                partners: ''
+            }
         }
     }
 </script>
+
+<style>
+    .headline {
+        text-align: center;
+        display: block;
+    }
+    .card-data {
+        font-size: 24px;
+    }
+    .v-card {
+        cursor: pointer;
+    }
+</style>
--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,26 @@
 <?php
 
-use Illuminate\Http\Request;
+    use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | is assigned the "api" middleware group. Enjoy building your API!
+    |
+    */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::resource('payments', 'API\PaymentController');
+    Route::resource('payments', 'API\PaymentController');
 
-Route::resource('partners', 'API\PartnerController');
+    Route::resource('partners', 'API\PartnerController');
 
-Route::post('/additional_data', [
-    'uses' => 'API\AdditionalDataController@get'
-]);
+    Route::post('/additional_data', ['uses' => 'API\AdditionalDataController@get']);
+
+    Route::get('/partners_count', ['uses' => 'API\PartnerController@partners_count']);
