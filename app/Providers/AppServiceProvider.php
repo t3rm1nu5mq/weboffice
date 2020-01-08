aade26f710398efbd13078b422c3b6a6e23bfa3c
Szabó András Ferenc <terminusmq@outlook.com>
2020-01-08 23:03:58 +0100

    Horizon added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Laravel\Horizon\Horizon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Horizon::auth(function ($request) {
+            if (Auth::check()) {
+                return true;
+            } 
+        });
     }
 }
