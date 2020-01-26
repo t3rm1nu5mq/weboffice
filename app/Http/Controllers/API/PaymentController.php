bbcee0c5fb2d97d189591a479eada21b363b110c
Szabó András Ferenc <terminusmq@outlook.com>
2020-01-27 00:56:11 +0100

    Payments management v0.1

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -1,99 +1,126 @@
 <?php
 
-namespace App\Http\Controllers\API;
+    namespace App\Http\Controllers\API;
 
-use App\Payment;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+    use App\Helpers\Helper;
+    use App\Invoice;
+    use App\Partner;
+    use App\Payment;
+    use Illuminate\Http\Request;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Support\Facades\DB;
 
-class PaymentController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return "Működik";
+    class PaymentController extends Controller {
+        /**
+         * Display a listing of the resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function index () {
+            return "Működik";
+        }
+
+        /**
+         * Show the form for creating a new resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function create () {
+            //
+        }
+
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param \Illuminate\Http\Request $request
+         * @return \Illuminate\Http\Response
+         */
+        public function store (Request $request) {
+            DB::beginTransaction();
+
+            $payment = new Payment; // fizetési tranzakció létrehozása
+            $payment->partner_id = $request->partner;
+            $payment->type = $request->type;
+            $payment->payment_method = $request->payment_method;
+            $payment->amount = $request->amount;
+            $payment->save();
+            $this->create_invoice($payment->id); // a fizetési tranzakcióhoz számla generálása
+
+            DB::commit();
+        }
+
+        /**
+         * Display the specified resource.
+         *
+         * @param int $id
+         * @return \Illuminate\Http\Response
+         */
+        public function show ($id) {
+            $payment = Payment::where('id', $id)->first();
+
+            return response()->json([
+                'data' => $payment
+            ]);
+        }
+
+        /**
+         * Show the form for editing the specified resource.
+         *
+         * @param int $id
+         * @return \Illuminate\Http\Response
+         */
+        public function edit ($id) {
+            //
+        }
+
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param \Illuminate\Http\Request $request
+         * @param int                      $id
+         * @return \Illuminate\Http\Response
+         */
+        public function update (Request $request, $id) {
+            //
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param int $id
+         * @return \Illuminate\Http\Response
+         */
+        public function destroy ($id) {
+            $payment = Payment::find($id);
+            $payment->delete();
+
+            return response()->json([
+                'message'     => 'A törlés sikeres.',
+                'result_code' => 200
+            ]);
+        }
+
+        ######################
+        ## fizetési típusok ##
+        ######################
+        public function create_payment () {
+
+        }
+
+        ######################
+        ## számlák kezelése ##
+        ######################--------------------------------------------------#
+        # Számlák létrehozására, megjelenítésére, törlésére szolgáló függvények #
+        #-----------------------------------------------------------------------#
+        // számla létrehozása
+        function create_invoice ($payment_id) {
+            $filename = $this->GenUID();
+            $invoice_number = Helper::create_invoice_number();
+
+            $invoice = new Invoice;
+            $invoice->payment_id = $payment_id;
+            $invoice->filename = $filename;
+            $invoice->invoice_number = $invoice_number;
+            $invoice->save();
+        }
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $payment = Payment::find($id);
-        $payment->delete();
-
-        return response()->json([
-            'message' => 'A törlés sikeres.',
-            'result_code' => 200
-        ]);
-    }
-
-    ######################
-    ## fizetési típusok ##
-    ######################
-    public function create_payment() {
-
-}
-}
--- resources/js/components/Payments.vue
+++ resources/js/components/Payments.vue
@@ -2,7 +2,53 @@
     <layout>
         <template v-slot:content>
             <div class="container">
-                <h2>Pénzügyek kezelése</h2>
+                <div class="row">
+                    <div class="col-md-9">
+                        <h2>Pénzügyek kezelése</h2>
+                        <div class="row infobox">
+                            <button class="btn btn-primary">
+                                <i class="fas fa-plus"></i> Új pénzügyi tranzakció
+                            </button>
+                            <button class="btn btn-primary">
+                                <i class="fas fa-balance-scale"></i> {{year}}. évi jelentés
+                            </button>
+                            <button class="btn btn-primary">
+                                <i class="fas fa-balance-scale"></i> {{month}}. havi jelentés
+                            </button><br>
+                            <button class="btn btn-primary">
+                                <i class="fas fa-archive"></i> Archívum
+                            </button>
+                            <button class="btn btn-primary" style="position: absolute; right: 35px">
+                                <i class="fas fa-search"></i> Keresés
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-md-3 infobox" style="margin-top: 53px; color: black; position: fixed; top: 92.5px; right: 50px">
+                        <h3 style="text-align: center">Statisztika</h3>
+                        <table>
+                            <tr>
+                                <th>E havi bevétel:</th>
+                                <td><i>0 Ft</i></td>
+                            </tr>
+                            <tr>
+                                <th>Múlt havi bevétel:</th>
+                                <td><i>0 Ft</i></td>
+                            </tr>
+                            <tr>
+                                <th>Negyed éves bevétel:</th>
+                                <td><i>0 Ft</i></td>
+                            </tr>
+                            <tr>
+                                <th>Eddigi éves bevétel:</th>
+                                <td><i>0 Ft</i></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <button class="btn btn-primary" style="position: fixed; top: 330px; right: 152.5px">
+                    <img src="/images/nav.jpg" style="width: 100px"><br>
+                    Ellenőrzés
+                </button>
             </div>
         </template>
     </layout>
@@ -16,10 +62,25 @@
 
         components: {
             layout
+        },
+
+        data() {
+            return {
+                year: new Date().getFullYear(),
+                month: new Date().getMonth() + 1
+            }
         }
     }
 </script>
 
 <style scoped>
-
+    .btn-primary {
+        margin: 5px;
+    }
+    td {
+        padding-left: 30px
+    }
+    .row {
+        margin-right: 15px
+    }
 </style>
