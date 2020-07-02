7697243778975d4a21564d9b0db1b9882f01f475
Szabó András Ferenc <terminusmq@outlook.com>
2020-07-02 23:23:06 +0200

    Frontend v0.2 - landing page CSS moved to app.scss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>TerminusMq WebOffice</title>
+    <title>TerminusMq Codes</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -12,67 +12,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 25px;
-            z-index: 1;
-        }
-
-        .top-left {
-            position: absolute;
-            left: 10px;
-            top: 18px;
-            z-index: 1;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
 </head>
+
 <body>
 <div class="flex-center position-ref full-height">
     <div class="top-left links">
@@ -88,10 +29,9 @@
                 <a class="main-header-button" href="{{ url('/home') }}">Home</a>
             @else
                 <a class="main-header-button" href="{{ route('login') }}">Bejelentkezés</a>
-
-                {{--                        @if (Route::has('register'))--}}
-                {{--                            <a href="{{ route('register') }}">Register</a>--}}
-                {{--                        @endif--}}
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Regisztráció</a>
+                @endif
             @endauth
         </div>
     @endif
